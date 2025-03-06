5db38dc70cf54ed46b67ed60cc3828ca9be6df1a
Guilherme Diniz dos Reis Gomes <gdrg@discente.ifpe.edu.br>
2025-03-06 18:56:47 -0300

    build da imagem

--- src/main/java/br/maxiprod/api_selecao/repository/RoleRepository.java
+++ src/main/java/br/maxiprod/api_selecao/repository/RoleRepository.java
@@ -2,9 +2,11 @@ package br.maxiprod.api_selecao.repository;
 
 import br.maxiprod.api_selecao.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(exported = false)
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(String name);
 }
--- src/main/java/br/maxiprod/api_selecao/repository/UserRepository.java
+++ src/main/java/br/maxiprod/api_selecao/repository/UserRepository.java
@@ -2,9 +2,11 @@ package br.maxiprod.api_selecao.repository;
 
 import br.maxiprod.api_selecao.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(exported = false)
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
@@ -17,4 +19,3 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Boolean existsByEmail(String email);
 }
-
